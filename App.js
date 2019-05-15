055f6b09e32090c59734a04a915edf66169b9914
guidoac <guido.ptr13@gmail.com>
2019-05-15 10:32:01 -0300

    ad

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, View, TextInput, Button } from 'react-native';
-import NavBar from './src/componentes/navbar';
-import { Container } from './src/componentes/container'
+import NavBarContainer from './src/componentes/navbar';
+import Container from './src/componentes/container'
 
 export default class App extends React.Component{
   render() {
     return (
       <Container>
-        <NavBar />
+        <NavBarContainer />
       </Container>
     );
   }
