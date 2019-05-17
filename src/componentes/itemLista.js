ab0a67e3b05a9c0b84d1b566554d5e7f3e2a2232
guidoac <guido.ptr13@gmail.com>
2019-05-17 00:13:06 -0300

    k

--- src/componentes/itemLista.js
+++ src/componentes/itemLista.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from 'styled-components/native';
 import React from 'react';
 import {StyleSheet} from 'react-native';
@@ -51,58 +50,4 @@ export default class ItemLista extends React.Component{
             </>
         )
     }
-=======
-import styled from 'styled-components/native';
-import React from 'react';
-import {StyleSheet} from 'react-native';
-
-const Item = styled.View`
-    height: 80px;
-    width: 100%;
-    border-bottom-color: black;
-    border-bottom-width: ${StyleSheet.hairlineWidth};
-    flex-direction: row;
-`
-const Thumb = styled.Image`
-    height: 100%;
-    position: relative;
-`
-const DetalhesVideo = styled.View`
-    align-items: center;
-    flex:1;
-    padding-top:5px;
-    position: relative;
-`
-
-const TituloVideo = styled.Text`
-    font-size: 20px;
-    margin-bottom: 10px;
-`
-
-const ButtonBaixarVideo = styled.TouchableOpacity`
-    flex:1;
-    width: 100%;
-    background-color: #c9c9c9;
-
-`
-
-export default class ItemLista extends React.Component{
-    render(){
-        return(
-            <>
-                <Item>
-                    <Thumb
-                    source={{uri:"https://img.youtube.com/vi/hHW1oY26kxQ/default.jpg"}}
-                    style={{aspectRatio:1.3,}}
-                    />
-                    <DetalhesVideo>
-                        <TituloVideo>Titulo do video aqui</TituloVideo>
-                        <ButtonBaixarVideo>
-                        </ButtonBaixarVideo>
-                    </DetalhesVideo>
-                </Item>
-            </>
-        )
-    }
->>>>>>> 16a0137447555c83a158a8207505f350122a3d30
 }
--- src/componentes/scrollViewContainer.js
+++ src/componentes/scrollViewContainer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from 'styled-components/native';
 import React from 'react';
 import DropDownVideo from './dropdownVideo';
@@ -33,40 +32,4 @@ export default class ScrollView extends React.Component{
             </>
         )
     }
-=======
-import styled from 'styled-components/native';
-import React from 'react';
-import DropDownVideo from './dropdownVideo';
-import ItemLista from './itemLista'
-
-const ScrollViewContainer = styled.ScrollView`
-    background-color: #dbdbdb;
-    position:relative;
-`
-
-export default class ScrollView extends React.Component{
-    render(){
-        return(
-            <>
-                <ScrollViewContainer>
-                    <DropDownVideo />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                    <ItemLista />
-                </ScrollViewContainer>
-            </>
-        )
-    }
->>>>>>> 16a0137447555c83a158a8207505f350122a3d30
 }
