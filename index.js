12e1ca6c945a7ab42b220542953188d255354564
guido <guido,ptr13@gmail.com>
2019-05-21 17:38:02 -0300

    att redux 2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * @format
  */
@@ -8,14 +7,3 @@ import App from './src';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-=======
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
->>>>>>> 25f708d177dfebac826020cdbfaec7920f51e332
